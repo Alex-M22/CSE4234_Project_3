4b5ae919d589f8d636a56ab298b1a0d3461b7419
anthonyciero82 <anthonyciero82@gmail.com>
2024-11-06 10:06:17 -0500

    cleared preferences bc button in nav

--- src/components/Preferences/Preferences.jsx
+++ src/components/Preferences/Preferences.jsx
@@ -3,12 +3,8 @@ import { useState } from 'react'
 function Preferences() {
   const [count, setCount] = useState(0)
 
-  return (
-    <> 
-     <button type="button" id="preferences" name="preferences" onClick=""> Preferences </button>
+    {/* Moved preferences button to Navigation. This is just for the actual card/menu */}
 
-    </>
-  )
 }
 
 export default Preferences
