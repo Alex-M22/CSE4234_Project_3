3531cc9ab5c2002c96c6bd37949de172b76e4e6a
anthonyciero82 <anthonyciero82@gmail.com>
2024-11-06 18:47:44 -0500

    Navigation preference button changes

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import './Navigation.css'
 
 function Navigation() {
     const [count, setCount] = useState(0)
 
     return (
         <>
-            <button type="button" id="preferences" name="preferences" onClick=""> Preferences </button>
+            <button type="button" id="preferences-button" name="preferences" onClick=""> Preferences </button>
 
         </>
     )
