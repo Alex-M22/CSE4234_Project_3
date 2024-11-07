af95d4100c1b035561d5c31d2111c6aa4b1be27f
skvlar <skyler.troi@gmail.com>
2024-11-07 10:35:36 -0500

    minor css changes

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -11,6 +11,13 @@ function ArticleCard(props) {
   const { topic, title, link, date, image, excerpt } = props.data;
   const mainUrl = new URL(link).origin;
 
+  const newDate = new Date(date).toLocaleDateString('en-US', {
+    weekday: 'long',  // "Monday"
+    year: 'numeric',  // "2024"
+    month: 'long',    // "November"
+    day: 'numeric'    // "7"
+  });
+
   return (
     <>
     <div className="article-card">
@@ -21,7 +28,7 @@ function ArticleCard(props) {
       <div className="article-content">
         <p className="article-url"><a href={link} target="_blank" rel="noopener noreferrer">{mainUrl}</a></p>
         <h2 className="article-title">{title}</h2>
-        <small className="article-date">Published on: {new Date(date).toLocaleDateString()}</small>
+        <small className="article-date">{newDate}</small>
         <p className="article-excerpt">{excerpt || "Something went wrong, please try again later."}</p>
       </div>
     </div>
